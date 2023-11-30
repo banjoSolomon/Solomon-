2d3dbc28d14f2da21f20d401f3aa0139d9a08e72
banjoSolomon <ayomidebanjo68@gmail.com>
2023-11-30 17:30:21 +0100

    java class work

--- Product.java
+++ Product.java
@@ -1,30 +1,15 @@
 import java.util.Scanner;
 public class Product{
 public static void main(String[] args){
-
 Scanner input = new Scanner(System.in);
-System.out.print("Enter first integer: ");
-int x = input.nextInt();
+System.out.print("Enter Product: ");
+String iteam = input.nextLine();
+System.out.print("Price: ");
+double price = input.nextDouble();
+System.out.print("Enter discount: ");
+double discount = input.nextDouble();
+double totalDiscount = (price - (price * discount * 0.01));
 
-System.out.print("Enter second integer: ");
-int y = input.nextInt();
-
-System.out.print("Enter third integer: ");
-int z = input.nextInt();
-
-int result = x * y * z;
-System.out.printf("product is %d%n", result);
-
-
-
-
+System.out.print("totalDiscount is :" + totalDiscount);
 }
-
-
-
-
-
-
-
-
 }

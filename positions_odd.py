51debedf6fcdbcf02b1988c7d2be7342ee53bfc9
banjoSolomon <151349000+banjoSolomon@users.noreply.github.com>
2023-12-11 08:42:15 +0100

    Add files via upload

---
+++ positions_odd.py
@@ -0,0 +1,9 @@
+def even_positions_func(myList):
+	for iteam in myList:
+		my_items = myList[1::2]
+	return my_items
+
+
+my_new_list = [9,11,5,-5,14,22,7,8]
+
+print(even_positions_func(my_new_list))	

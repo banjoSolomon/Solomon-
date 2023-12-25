98120a258d571dcb3d4b88e05eeb43ecc566ae08
banjoSolomon <151349000+banjoSolomon@users.noreply.github.com>
2023-12-25 11:01:22 +0100

    Add files via upload

---
+++ EvenArray.java
@@ -0,0 +1,23 @@
+public class EvenArray{
+
+public static void main(String[] args){
+int[] index = {2, 5, 16, 18, 21 };
+
+System.out.println("The even numbers of array: ");
+for(int i = 0; i < index.length; i++){
+
+if(index[i] % 2 == 0){
+System.out.print(index[i] + " ");
+}
+
+
+}
+
+
+  }
+
+
+//System.out.println();
+
+
+}
---
+++ OddNumbers.java
@@ -0,0 +1,21 @@
+public class OddNumbers{
+
+	public static void main(String[] args){
+	int[] list = {23, 30, 35, 22, 66, 45, 23, 56, 100};
+	
+	System.out.println("The odd number of the array is : ");
+	for(int i = 0; i < list.length; i++){
+	if(list[i] % 2 != 0){
+	System.out.print(list[i] + " ");
+      }
+
+
+
+         }
+
+
+    }
+
+
+
+}

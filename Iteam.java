63fed5efac58fc0aaa4f50f142de8767c591dc64
banjoSolomon <151349000+banjoSolomon@users.noreply.github.com>
2024-01-05 19:18:37 +0100

    Add files via upload

---
+++ Iteam.java
@@ -0,0 +1,33 @@
+public class Iteam{
+
+private String itemBought;
+private  int price;
+private double quntity;
+private int totalAmount;
+
+
+	public Account(String itemBought, int price, double quntity, int totalAmount){
+	this.name = name;
+	this.price = price;
+	this.quntity = quntity;
+	this.totalAmount = totalAmount;
+}
+
+	public String getItemBought(){
+	return itemBought;
+        }
+	
+	public int getPrice(){
+
+    	return price;
+       }
+	public double getQuantity(){
+	return quantity;
+
+       }
+	public int getTotalAmount(){
+	return totalAmount;
+         }
+	
+
+}
